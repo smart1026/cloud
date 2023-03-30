58295c6dd9723417e54295dc6dddcbb19fb49aa7
caijiaxing <caijiaxing1992@163.com>
2023-03-30 21:27:25 +0800

    添加common模块  实现entity封装

---
+++ cloud-api-common/src/main/java/com/atguigu/spring/entities/Payment.java
@@ -0,0 +1,29 @@
+package com.atguigu.spring.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @Author: 蔡佳兴
+ * @createTime: 2023年03月30日 19:04:14
+ * @version: 1.0
+ * @Description:
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+
+    /**
+     * 主键
+     */
+    private Long id;
+    /**
+     * 支付流水号
+     */
+    private String serial;
+
+}
