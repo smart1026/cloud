e25328ed4ec3b53cf1925767c5273788553e932b
caijiaxing <caijiaxing1992@163.com>
2023-03-31 19:50:48 +0800

    新增eureka server2服务  搭建eureka服务集群

---
+++ cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: 蔡佳兴
+ * @createTime: 2023年03月30日 22:46:58
+ * @version:
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+
+}
