1f68a594d2b37d89b1b2d0c1eed1d328e48330bb
caijiaxing <caijiaxing1992@163.com>
2023-04-07 20:54:45 +0800

    hystrix消费模块 添加@@DefaultProperties注解 配置全局异常处理方法

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/study/spring/controller/OrderHyrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/study/spring/controller/OrderHyrixController.java
@@ -1,5 +1,6 @@
 package com.study.spring.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.study.spring.entities.CommonResult;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Slf4j
 @RequestMapping("/orderHystrixFeign")
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderHyrixController {
 
     @Autowired
@@ -29,19 +31,32 @@ public class OrderHyrixController {
 
 
     @GetMapping(value = "/getPaymentById")
-    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
-    })
     public Object getPaymentById(@RequestParam("id") Long id){
         return paymentHystrixService.paymentInfo_OK(id);
+    }
+
+
+    @GetMapping(value = "/timeOut")
+     /*@HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    })*/
+    @HystrixCommand
+    public Object getPaymentTimeOut(@RequestParam("id") Long id){
+        return paymentHystrixService.paymentInfo_TimeOut(id);
     }
 
     public String paymentTimeOutFallbackMethod(@RequestParam("id") Long id) {
         return "我是消费者80,对方支付系统繁忙请10秒种后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
     }
 
-    @GetMapping(value = "/timeOut")
-    public Object getPaymentTimeOut(@RequestParam("id") Long id){
-        return paymentHystrixService.paymentInfo_TimeOut(id);
+
+    /**
+     * 全局fallback
+     *
+     * @return
+     */
+    public String payment_Global_FallbackMethod() {
+        return "Global异常处理信息,请稍后重试.o(╥﹏╥)o";
     }
+
 }
