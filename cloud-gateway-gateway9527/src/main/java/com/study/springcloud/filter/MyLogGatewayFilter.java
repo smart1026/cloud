84e6445624ca63d18d8a6ec0a0b1ce4fd175df74
caijiaxing <caijiaxing1992@163.com>
2023-04-10 21:59:34 +0800

    添加gateway GlobalFilter自定义全局配置

---
+++ cloud-gateway-gateway9527/src/main/java/com/study/springcloud/filter/MyLogGatewayFilter.java
@@ -0,0 +1,49 @@
+package com.study.springcloud.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @Author: 蔡佳兴
+ * @createTime: 2023年04月10日 21:57:08
+ * @version:
+ * @Description:
+ */
+@Component
+@Slf4j
+public class MyLogGatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("come in global filter: {}", new Date());
+
+        ServerHttpRequest request = exchange.getRequest();
+        String uname = request.getQueryParams().getFirst("username");
+        if (uname == null) {
+            log.info("用户名为null，非法用户");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        // 放行
+        return chain.filter(exchange);
+    }
+
+    /**
+     * 过滤器加载的顺序 越小,优先级别越高
+     *
+     * @return
+     */
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+
+}
