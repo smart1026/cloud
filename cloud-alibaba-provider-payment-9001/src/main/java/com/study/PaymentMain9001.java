f13f1b4e4b1961ea1f665e5af71b558e5ea152c3
smart6135 <caijiaxing1992@163.com>
2023-04-13 21:26:07 +0800

    新建nacos9001 9002服务提供者模块

---
+++ cloud-alibaba-provider-payment-9001/src/main/java/com/study/PaymentMain9001.java
@@ -0,0 +1,22 @@
+package com.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: 蔡佳兴
+ * @createTime: 2023年04月13日 21:16:35
+ * @version:
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+
+}
---
+++ cloud-alibaba-provider-payment-9002/src/main/java/com/study/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.study.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 蔡佳兴
+ * @createTime: 2023年04月13日 21:18:15
+ * @version:
+ * @Description:
+ */
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@RequestParam("id") Integer id) {
+        return "nacos registry, serverPort: "+ serverPort+"\t id"+id;
+    }
+
+}
